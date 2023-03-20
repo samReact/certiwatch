deac775e619642d37bf4c66fb5b85f8271b25704
samReact <sam.react001@gmail.com>
2023-03-20 19:45:56 +0100

    :construction: routeur + refacto

--- client/src/AppBar.jsx
+++ client/src/AppBar.jsx
@@ -7,11 +7,22 @@ import logo from './assets/logo-main.png';
 export default function ResponsiveAppBar() {
   const navigate = useNavigate();
   return (
-    <Row justify={'space-between'} align="middle" style={{ padding: 12 }}>
-      <div sx={{ flexGrow: 1 }}>
-        <img src={logo} alt="logo" width={200} onClick={() => navigate('/')} />
-      </div>
-      <LogIn />
-    </Row>
+    <div style={{ boxShadow: '0px 1px 5px #a5a5a5' }}>
+      <Row
+        justify={'space-between'}
+        align="middle"
+        style={{ padding: 12, boxShadow: '10px' }}
+      >
+        <div sx={{ flexGrow: 1 }}>
+          <img
+            src={logo}
+            alt="logo"
+            width={200}
+            onClick={() => navigate('/')}
+          />
+        </div>
+        <LogIn />
+      </Row>
+    </div>
   );
 }
--- client/src/routes/SellPage.jsx
+++ client/src/routes/SellPage.jsx
@@ -1,10 +1,35 @@
+import { Form, Input, Button, Select, Row, Col } from 'antd';
 import React from 'react';
 import Stepper from '../Stepper';
+
+const { Item } = Form;
 
 export default function SellPage() {
   return (
     <div className="container">
       <Stepper />
+      <div className="container-content">
+        <Row>
+          <Col xs={24} md={12}>
+            <Form name="form_item_path" layout="vertical">
+              <Item name="brand" label="Marque">
+                <Select>
+                  <Select.Option value="demo">Demo</Select.Option>
+                </Select>
+              </Item>
+              <Item name="model" label="Modèle">
+                <Input />
+              </Item>
+              <Item name="year" label="Année">
+                <Input />
+              </Item>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 }
