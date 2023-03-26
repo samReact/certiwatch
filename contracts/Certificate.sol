232611d3d3a9d8b883c3bf094a9fbd89a17c2f6c
samReact <sam.react001@gmail.com>
2023-03-26 13:11:49 +0200

    rename nft contract + script improvment

---
+++ contracts/Certificate.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract Certificate is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter public tokenIds;
+
+    // uint public tokenCount = _tokenIds.current();
+
+    constructor() ERC721("Certiwatch certificates", "CWT") {}
+
+    function mintItem(string memory _tokenURI) public returns (uint256) {
+        tokenIds.increment();
+        _safeMint(msg.sender, tokenIds.current());
+        _setTokenURI(tokenIds.current(), _tokenURI);
+        return tokenIds.current();
+    }
+}
--- scripts/01_deploy.js
+++ scripts/01_deploy.js
@@ -4,15 +4,39 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
+const { artifacts } = require('hardhat');
 const hre = require('hardhat');
 
 async function main() {
-  const Watches = await hre.ethers.getContractFactory('Watches');
-  const watches = await Watches.deploy();
+  const Certificate = await hre.ethers.getContractFactory('Certificate');
+  const certificate = await Certificate.deploy();
 
-  await watches.deployed();
+  await certificate.deployed();
 
-  console.log(`Watches deployed to ${watches.address}`);
+  saveClientFiles(certificate, 'Certificate');
+
+  console.log(`Certificate deployed to ${certificate.address}`);
+}
+
+function saveClientFiles(contract, name) {
+  const fs = require('fs');
+  const contractsDir = __dirname + '/../client/contractsData';
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  fs.writeFileSync(
+    contractsDir + `/${name}-address.json`,
+    JSON.stringify({ address: contract.address }, undefined, 2)
+  );
+
+  const contractArtifact = artifacts.readArtifactSync('Certificate');
+
+  fs.writeFileSync(
+    contractsDir + `/${name}.json`,
+    JSON.stringify(contractArtifact, null, 2)
+  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
