36adaa378a86633ce8ff9ac2bfe4b4fe501326b6
samReact <sam.react001@gmail.com>
2023-03-31 09:40:38 +0200

    add  front-end eslint config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,45 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
-const nativeNodeModulesPlugin = {
-  name: 'native-node-modules',
-  setup(build) {
-    // If a ".node" file is imported within a module in the "file" namespace, resolve
-    // it to an absolute path and put it into the "node-file" virtual namespace.
-    build.onResolve({ filter: /\.node$/, namespace: 'file' }, (args) => ({
-      path: require.resolve(args.path, { paths: [args.resolveDir] }),
-      namespace: 'node-file'
-    }));
-
-    // Files in the "node-file" virtual namespace call "require()" on the
-    // path from esbuild of the ".node" file in the output directory.
-    build.onLoad({ filter: /.*/, namespace: 'node-file' }, (args) => ({
-      contents: `
-        import path from ${JSON.stringify(args.path)}
-        try { module.exports = require(path) }
-        catch {}
-      `
-    }));
-
-    // If a ".node" file is imported within a module in the "node-file" namespace, put
-    // it in the "file" namespace where esbuild's default loading behavior will handle
-    // it. It is already an absolute path since we resolved it to one above.
-    build.onResolve({ filter: /\.node$/, namespace: 'node-file' }, (args) => ({
-      path: args.path,
-      namespace: 'file'
-    }));
-
-    // Tell esbuild's default loading behavior to use the "file" loader for
-    // these ".node" files.
-    let opts = build.initialOptions;
-    opts.loader = opts.loader || {};
-    opts.loader['.node'] = 'file';
-  }
-};
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint()],
   server: {
     proxy: {
       '/api': {
@@ -49,8 +14,4 @@ export default defineConfig({
       }
     }
   }
-  // optimizeDeps: { exclude: ['fsevents', '@nomicfoundation'] },
-  // define: {
-  //   'process.env': {}
-  // }
 });
