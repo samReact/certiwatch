7db3b778f9eb77c2e47e232c1ec2d3cd4a2aa5a5
samReact <sam.react001@gmail.com>
2023-04-04 10:06:36 +0200

    add jwt to api

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const services = require('./services.js');
+var { expressjwt: jwt } = require('express-jwt');
+
 require('dotenv').config();
+
+const secret = process.env.JWT_SECRET;
 
 const app = express();
 const port = 5000;
@@ -10,23 +14,32 @@ app.use(cors());
 app.use(express.json({ limit: '100000mb' }));
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/api/uploadImages', async (req, res) => {
-  try {
-    const result = await services.uploadImages(req, res);
-    res.send(result);
-  } catch (error) {
-    throw error;
+app.post(
+  '/api/uploadImages',
+  jwt({ secret, algorithms: ['HS256'] }),
+  async (req, res) => {
+    try {
+      const result = await services.uploadImages(req, res);
+      res.send(result);
+    } catch (error) {
+      throw error;
+    }
   }
-});
+);
 
-app.post('/api/uploadIpfs', async (req, res) => {
-  try {
-    await services.fillPng(req, res);
-    await services.pinFileToIPFS(req, res);
-  } catch (error) {
-    throw error;
+app.post(
+  '/api/uploadIpfs',
+  jwt({ secret, algorithms: ['HS256'] }),
+  async (req, res) => {
+    res.send('ok');
+    // try {
+    //   await services.fillPng(req, res);
+    //   await services.pinFileToIPFS(req, res);
+    // } catch (error) {
+    //   throw error;
+    // }
   }
-});
+);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
