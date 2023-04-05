8c6f87f93f9e9a9a949aadf5c4e5927c1d3ca744
samReact <sam.react001@gmail.com>
2023-04-05 19:12:40 +0200

    add sepolia config

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -10,27 +10,31 @@ module.exports = {
   networks: {
     localhost: {
       url: 'http://127.0.0.1:8545',
-      chainId: 31337,
-      gas: 3000
+      chainId: 31337
     },
     goerli: {
       url: `https://goerli.infura.io/v3/${process.env.INFURA_ID}`,
       accounts: {
         mnemonic: `${process.env.MNEMONIC}`
       },
-      gas: 3000,
       chainId: 5
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_ID}`,
+      accounts: {
+        mnemonic: `${process.env.MNEMONIC}`
+      },
+      chainId: 11155111
     }
   },
-
   gasReporter: {
     enabled: true
   },
   solidity: '0.8.19',
   settings: {
     optimizer: {
-      enabled: false,
-      runs: 400
+      enabled: true,
+      runs: 200
     }
   }
 };
