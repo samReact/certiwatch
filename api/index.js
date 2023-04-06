8787f4f6a055047bbb135c2fd06f4ea1ef749155
samReact <sam.react001@gmail.com>
2023-04-06 20:11:21 +0200

    test

--- api/index.js
+++ api/index.js
@@ -8,7 +8,10 @@ require('dotenv').config();
 const app = express();
 const port = 5000;
 
-// app.use(cors());
+app.use(cors());
+app.options('/*', (_, res) => {
+  res.sendStatus(200);
+});
 app.use(express.json({ limit: '100000mb' }));
 app.use(express.urlencoded({ extended: true }));
 
