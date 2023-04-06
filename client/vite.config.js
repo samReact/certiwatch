587c562acb9d703069316bf0aae7f660be44c83e
samReact <sam.react001@gmail.com>
2023-04-06 08:42:35 +0200

    modify vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,21 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 
-export default defineConfig(({ mode }) => {
-  const isDev = mode === 'development';
-  console.log('mode:', mode);
-  return {
-    plugins: [react(), eslint()],
-    server: {
-      proxy: {
-        '/api': {
-          target: isDev
-            ? 'http://localhost:5000'
-            : 'https://certiwatch.vercel.app',
-          changeOrigin: true,
-          secure: !isDev
-        }
-      }
-    }
-  };
+export default defineConfig({
+  plugins: [react(), eslint()]
 });
