b8d5fd55a6d76c275e8aef5e470beba6bee68386
samReact <sam.react001@gmail.com>
2023-04-06 12:21:15 +0200

    add cors origin

--- api/index.js
+++ api/index.js
@@ -8,7 +8,11 @@ require('dotenv').config();
 const app = express();
 const port = 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: 'https://certiwatch-psi.vercel.app'
+  })
+);
 app.use(express.json({ limit: '100000mb' }));
 app.use(express.urlencoded({ extended: true }));
 
