61f4dedbe1bfce2309e8c468541a2ad24bf0695a
samReact <sam.react001@gmail.com>
2023-04-06 12:27:46 +0200

    revert change

--- api/index.js
+++ api/index.js
@@ -8,11 +8,7 @@ require('dotenv').config();
 const app = express();
 const port = 5000;
 
-app.use(
-  cors({
-    origin: 'https://certiwatch-psi.vercel.app'
-  })
-);
+app.use(cors());
 app.use(express.json({ limit: '100000mb' }));
 app.use(express.urlencoded({ extended: true }));
 
