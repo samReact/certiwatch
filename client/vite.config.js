2686acc907afbeb217d5e5996819bc98ca43d1cf
samReact <sam.react001@gmail.com>
2023-04-06 05:58:25 +0200

    add prod proxy setting

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,15 +2,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 
-export default defineConfig({
-  plugins: [react(), eslint()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development';
+  return {
+    plugins: [react(), eslint()],
+    server: {
+      proxy: {
+        '/api': {
+          target: isDev
+            ? 'http://localhost:5000'
+            : 'https://certiwatch.vercel.app/',
+          changeOrigin: isDev,
+          secure: !isDev
+        }
       }
     }
-  }
+  };
 });
