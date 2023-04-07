f8ede490bb32b880152f177902be0365ede6ced3
samReact <sam.react001@gmail.com>
2023-04-07 05:50:32 +0200

    test

--- api/index.js
+++ api/index.js
@@ -8,9 +8,17 @@ require('dotenv').config();
 const app = express();
 const port = 5000;
 
-app.use(cors());
 app.use(express.json({ limit: '100000mb' }));
 app.use(express.urlencoded({ extended: true }));
+
+const corsOptions = {
+  origin: 'https://certiwatch-psi.vercel.app',
+  methods: ['OPTIONS', 'GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+  maxAge: 86400
+};
+
+app.use(cors(corsOptions));
 
 function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization'];
@@ -30,7 +38,7 @@ function generateAccessToken(user) {
 
 app.post('/api/access', (req, res) => {
   const token = generateAccessToken({ user: req.body.user });
-  response.writeHead(200, {
+  res.writeHead(200, {
     'Content-Type': 'application/json; charset=utf-8'
   });
 
